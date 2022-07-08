789a951ed35da34222163317897d8af4a734d58b
Joanssebas <yoandec@hotmail.com>
2022-07-08 08:22:11 -0500

    validacion personalizada

--- src/validations/validationSchema.js
+++ src/validations/validationSchema.js
@@ -1,23 +1,26 @@
-import * as yup from "yup";
-
 const UserLoginSchemaValidator = (model) => {
   const error = {};
 
+  const validateEmail = (email) => {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
+
   if (!model.login) {
-    error.login = "Nombre de usuario es requerido";
-  } else if (model.login.length < 5) {
-    error.login = "El nombre debe contener al menos 5 caracteres";
+    error.login = "El correo es obligatorio";
+  } else if (!validateEmail(model.login)) {
+    error.login = "El correo no es valido";
   }
 
   if (!model.password1) {
-    error.password1 = "Se requiere residencia";
-  } else if (model.password1.length < 10) {
-    error.login = "Password has to be at least 10 characters long!";
+    error.password1 = "La contraseña es obligatoria";
+  } else if (model.password1.length < 5) {
+    error.login = "La contraseña debe ser de almenos 5 caracteres";
   }
 
-  // if (model.password1 !== model.password2) {
-  //   error.password1 = "Passwords mismatch!";
-  // }
+  if (model.password1 !== model.password2) {
+    error.password1 = "No coinciden las contraseñas";
+  }
 
   if (Object.keys(error).length) {
     return error;
